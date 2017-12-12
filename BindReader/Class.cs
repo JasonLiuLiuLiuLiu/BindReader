d72903791d24ee9a58cf869d473d2b35e2b2eb83
刘振宇 <liuzhenyulive@live.com>
2017-12-12 21:14:16 +0800

    Bind读取配置文件到C#实例

---
+++ BindReader/Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BindReader
+{
+    public class Class
+    {
+        public String Name { get; set; }
+        public List<Item> Items { get; set; }
+    }
+    public class Item
+    {
+        public string Language { get; set; }
+        public string Tool { get; set; }
+    }
+}
--- BindReader/Startup.cs
+++ BindReader/Startup.cs
@@ -5,12 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BindReader
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,6 +35,18 @@ namespace BindReader
 
             app.Run(async (context) =>
             {
+                //var con = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                //Configuration = con.Build();
+                var myClass = new Class();
+               
+                Configuration.Bind(myClass);
+                Console.WriteLine($"name:{myClass.Name}");
+                Console.WriteLine();
+                for (int i = 0; i < myClass.Items.Count; i++)
+                {
+                    await context.Response.WriteAsync($"language:{myClass.Items[i].Language}");
+                    await context.Response.WriteAsync($"tool:{myClass.Items[i].Tool}");
+                }
                 await context.Response.WriteAsync("Hello World!");
             });
         }
